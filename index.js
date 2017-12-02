f9a37cac475a2bda4c41b217b69a04933a8b85c7
Przemek Gałdys <galdys.przemek@gmail.com>
2017-12-02 13:41:03 +0100

    Zadanie 13.8

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ var fs = require('fs');
 var server = http.createServer();
 
 server.on('request', function(request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && (request.url === '/')) {
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
         fs.readFile('./index.html', 'utf-8', function(err, data) {
             response.write(data);
             response.end();
         });
     } else {
         response.statusCode = 404;
-        fs.readFile('./404.html', 'utf-8', function(err, data) {
+        response.setHeader("Content-Type", "image/jpg");
+        fs.readFile('./error.jpg', function(err, data) {
             response.write(data);
             response.end();
         });
     }
 });
 
-server.listen(8080);
+server.listen(7070);
